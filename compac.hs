8ef2f83627b3ab4381014b4c0bac79c4bd64d752
João Víctor Paiva Nunes Pereira <joaov.pnp@gmail.com>
2024-08-24 07:55:32 -0300

    Tornando Tree uma estrutura genérica

--- compac.hs
+++ compac.hs
@@ -3,7 +3,7 @@ import System.IO
 type Word' = String
 type Quantity = Integer
 
-data Tree = Node (Word', Quantity) Tree Tree | Leaf deriving Show
+data Tree a = Node a Tree Tree | Leaf deriving Show
 
 main = do putStr("Digite o diretorio do arquivo: ")
           hFlush stdout
