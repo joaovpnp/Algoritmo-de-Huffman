985cf8e3293754c060f76622ab187ae768e550de
João Víctor Paiva Nunes Pereira <joaov.pnp@gmail.com>
2024-08-24 01:27:01 -0300

    Iniciada logica da main

--- compac.hs
+++ compac.hs
@@ -4,3 +4,10 @@ type Word' = String
 type Quantity = Integer
 
 data Tree = Node (Word', Quantity) Tree Tree | Leaf deriving Show
+
+main = do putStr("Digite o diretorio do arquivo: ")
+          hFlush stdout
+          arq <- getLine
+          txt <- readFile
+
+
