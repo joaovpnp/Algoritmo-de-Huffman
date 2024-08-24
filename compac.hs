3638e2ee78282fbbd5680457a9cd0698a1fcb01f
João Víctor Paiva Nunes Pereira <joaov.pnp@gmail.com>
2024-08-24 08:25:28 -0300

    Criando árvore de palavras

--- compac.hs
+++ compac.hs
@@ -3,11 +3,18 @@ import System.IO
 type Word' = String
 type Quantity = Integer
 
-data Tree a = Node a Tree Tree | Leaf deriving Show
+data Tree a = Node a (Tree a) (Tree a) | Leaf deriving Show
 
 main = do putStr("Digite o diretorio do arquivo: ")
           hFlush stdout
           arq <- getLine
           txt <- readFile
 
+wordsTree c Leaf = Node (c, 1) Leaf Leaf
+wordsTree c (Node (letter, q) left right)
+    | c == letter = Node (letter, q+1) left right
+    | c < letter = Node (letter, q) (wordsTree c left) right
+    | otherwise = Node (letter, q) left (wordsTree c right)
+
+countWords cs = foldr wordsTree Leaf cs
 
